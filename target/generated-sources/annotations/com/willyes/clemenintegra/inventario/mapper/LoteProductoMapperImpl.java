443037c69a23ae79150b3c69bc3bc2fea8e59fcc
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2025-06-16 19:14:41 -0500

    Se insertaron las tablas data.sql y shema.sql para pruebas unitarias

--- target/generated-sources/annotations/com/willyes/clemenintegra/inventario/mapper/LoteProductoMapperImpl.java
+++ target/generated-sources/annotations/com/willyes/clemenintegra/inventario/mapper/LoteProductoMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-16T18:54:38-0500",
+    date = "2025-06-16T19:05:30-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
