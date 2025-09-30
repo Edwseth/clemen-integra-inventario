e04f16d530d7c987439112f75d19c736c52dfac5
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2025-09-30 15:26:37 -0500

    chore(openapi): elimina server localhost para permitir inferencia de host/esquema detrás de túnel

--- src/main/java/com/willyes/clemenintegra/shared/config/OpenAPIConfig.java
+++ src/main/java/com/willyes/clemenintegra/shared/config/OpenAPIConfig.java
@@ -2,11 +2,9 @@ package com.willyes.clemenintegra.shared.config;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.*;
-import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -20,8 +18,7 @@ public class OpenAPIConfig {
                         .description("Documentación de los servicios REST para los módulos de Clemen-Integra")
                         .contact(new Contact()
                                 .name("Will Yes Solutions")
-                                .email("soporte@willyessolutions.com")))
-                .servers(List.of(new Server().url("http://localhost:8080")));
+                                .email("soporte@willyessolutions.com")));
     }
 }
 
