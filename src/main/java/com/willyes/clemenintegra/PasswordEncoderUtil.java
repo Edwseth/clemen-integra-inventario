6576d612db0bab4cd5f53148ba9faf8e84dd208c
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2025-10-02 08:57:25 -0500

    Se ajustan permisos de ingreso

--- src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
+++ src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
@@ -7,7 +7,7 @@ public class PasswordEncoderUtil {
 
     public static void main(String[] args) {
         // Cambia esta línea con la contraseña que quieres codificar
-        String rawPassword = "homeopaticos123";
+        String rawPassword = "WNE2831";
 
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         String encodedPassword = encoder.encode(rawPassword);
