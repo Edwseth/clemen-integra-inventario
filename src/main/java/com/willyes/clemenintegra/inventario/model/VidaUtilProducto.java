bc9c24772cf36fe3b2727b27316a30bddc560afc
Edwin Serrano <edwseth.77@gmail.com>
2025-11-17 11:45:34 -0500

    Fix VidaUtilProducto creation and update flow

--- src/main/java/com/willyes/clemenintegra/inventario/model/VidaUtilProducto.java
+++ src/main/java/com/willyes/clemenintegra/inventario/model/VidaUtilProducto.java
@@ -18,8 +18,7 @@ public class VidaUtilProducto {
     private Integer productoId;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @MapsId
-    @JoinColumn(name = "producto_id", foreignKey = @ForeignKey(name = "fk_vida_util_productos_producto"))
+    @JoinColumn(name = "producto_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_vida_util_productos_producto"))
     private Producto producto;
 
     @Column(name = "semanas_vigencia", nullable = false)
