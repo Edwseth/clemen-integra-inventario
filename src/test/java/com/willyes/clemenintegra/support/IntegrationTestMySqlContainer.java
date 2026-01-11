3adf8e1487f922fac3af067ccfdda12760af277e
Edwin Serrano <edwseth.77@gmail.com>
2026-01-11 18:29:01 -0500

    Ajustar test de ordenes de produccion a H2

--- src/test/java/com/willyes/clemenintegra/support/IntegrationTestMySqlContainer.java
+++ src/test/java/com/willyes/clemenintegra/support/IntegrationTestMySqlContainer.java
@@ -6,9 +6,11 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+// Activa TestcontainersExtension para los tests que necesitan Docker (vía @Testcontainers).
 @Testcontainers
 public abstract class IntegrationTestMySqlContainer {
 
+    // La presencia del @Container registra y arranca el GenericContainer.
     @Container
     static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("clemen_test")
