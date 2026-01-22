9f8bc090583d0a6c6fc386393d429622df15ab77
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2026-01-21 21:15:40 -0500

    Se ajusta la migración

--- src/main/resources/db/migration/inventario/V20251241__etapa_plantilla_unique_constraints.sql
+++ src/main/resources/db/migration/inventario/V20251241__etapa_plantilla_unique_constraints.sql
@@ -1,5 +1,2 @@
 alter table etapa_plantilla
-    add constraint uk_etapa_plantilla_producto_secuencia unique (producto_id, secuencia);
-
-alter table etapa_plantilla
     add constraint uk_etapa_plantilla_producto_nombre unique (producto_id, nombre);
