18b1cd47f72ee2bfe15cecd5fc3ca2ff36026f36
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2026-01-29 15:31:20 -0500

    nada

--- src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
+++ src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
@@ -9,7 +9,7 @@ public class PasswordEncoderUtil {
 
     public static void main(String[] args) {
         // Cambia esta línea con la contraseña que quieres codificar
-        String rawPassword = "YP202";
+        String rawPassword = "263419";
 
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         String encodedPassword = encoder.encode(rawPassword);
