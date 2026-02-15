0a2a853d89c5b450f37a87bf674b7d3ffa2a7b8f
Edwin Serrano <edwseth.77@gmail.com>
2026-02-15 11:39:33 -0500

    Soporta alias JSON module en update de permisos RBAC

--- src/main/java/com/willyes/clemenintegra/shared/security/dto/rbac/UpdateRolPermisosRequest.java
+++ src/main/java/com/willyes/clemenintegra/shared/security/dto/rbac/UpdateRolPermisosRequest.java
@@ -1,8 +1,10 @@
 package com.willyes.clemenintegra.shared.security.dto.rbac;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
-public record UpdateRolPermisosRequest(@NotNull List<Long> permisoIds, @NotBlank String modulo) {
+public record UpdateRolPermisosRequest(@NotNull List<Long> permisoIds,
+                                       @JsonAlias("module") @NotBlank String modulo) {
 }
