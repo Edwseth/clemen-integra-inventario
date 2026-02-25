4c6fba59b75cc57f3aa15af912d84d17217094cc
Edwin Serrano <edwseth.77@gmail.com>
2026-02-25 18:32:27 -0500

    Stabilize security test harness and regularization salida flow

--- src/test/java/com/willyes/clemenintegra/support/TestAuth.java
+++ src/test/java/com/willyes/clemenintegra/support/TestAuth.java
@@ -14,6 +14,31 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 public final class TestAuth {
 
+    private static final String[] SUPER_ADMIN_AUTHORITIES = {
+            "ROLE_SUPER_ADMIN", "ROL_SUPER_ADMIN",
+            "INV_READ", "INV_WRITE", "INV_EXPORT", "INV_WORKFLOW", "INV_WORKFLOW_START", "INV_WORKFLOW_FINISH", "INV_DECIDE",
+            "INV_CONTEOS_READ", "INV_CONTEOS_WRITE", "INV_CONTEOS_APPLY", "INV_CONTEOS_CLOSE", "INV_CONTEOS_START",
+            "INV_AJUSTES_READ", "INV_AJUSTES_WRITE", "INV_MOVIMIENTOS_READ", "INV_LOTES_READ", "INV_KARDEX_READ",
+            "INV_SOLICITUDES_READ", "INV_SOLICITUDES_WRITE", "INV_ALERTAS_READ",
+            "QC_READ", "QC_WRITE", "QC_EXPORT", "QC_WORKFLOW", "QC_WORKFLOW_FINISH", "QC_DECIDE", "QC_ALERTAS_READ",
+            "PROD_READ", "PROD_WRITE", "PROD_WORKFLOW", "PROD_WORKFLOW_FINISH", "PROD_DECIDE",
+            "PO_READ", "PO_WRITE", "PO_EXPORT", "PO_WORKFLOW", "PO_WORKFLOW_FINISH", "PO_DECIDE",
+            "DOC_READ", "DOC_WRITE", "DOC_DELETE", "ADMIN_RBAC_READ", "ADMIN_RBAC_WRITE",
+            "BOM_READ", "BOM_WRITE", "BOM_FORMULA_READ", "BOM_FORMULA_WRITE"
+    };
+
+    private static final String[] CONTADOR_AUTHORITIES = {
+            "INV_READ", "INV_CONTEOS_READ", "INV_CONTEOS_WRITE", "INV_CONTEOS_APPLY", "INV_CONTEOS_CLOSE", "INV_CONTEOS_START"
+    };
+
+    private static final String[] JEFE_CALIDAD_AUTHORITIES = {
+            "QC_READ", "QC_WRITE", "QC_WORKFLOW", "QC_WORKFLOW_FINISH", "QC_DECIDE"
+    };
+
+    private static final String[] PLANEADOR_AUTHORITIES = {
+            "PO_READ", "PO_WRITE", "PO_MRP_READ", "PO_MRP_WRITE", "PO_PLAN_SEMANAL_READ", "PO_PLAN_SEMANAL_WRITE"
+    };
+
     private TestAuth() {
     }
 
@@ -52,6 +77,22 @@ public final class TestAuth {
 
     public static RequestPostProcessor authMixed(String username, String[] authorities, String[] roles) {
         return authWithPermissionsAndRoles(username, authorities, roles);
+    }
+
+    public static RequestPostProcessor asSuperAdmin() {
+        return auth("super-admin", SUPER_ADMIN_AUTHORITIES);
+    }
+
+    public static RequestPostProcessor asContador() {
+        return auth("contador", CONTADOR_AUTHORITIES);
+    }
+
+    public static RequestPostProcessor asJefeCalidad() {
+        return auth("jefe-calidad", JEFE_CALIDAD_AUTHORITIES);
+    }
+
+    public static RequestPostProcessor asPlaneador() {
+        return auth("planeador", PLANEADOR_AUTHORITIES);
     }
 
     private static List<GrantedAuthority> toGrantedAuthorities(String... values) {
