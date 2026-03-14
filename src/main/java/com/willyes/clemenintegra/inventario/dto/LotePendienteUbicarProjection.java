6f8585a95825ecb4c2ca720730216d460e37648a
Edwin Serrano <edwseth.77@gmail.com>
2026-03-14 17:29:13 -0500

    Restaurar contrato completo en pendientes-ubicar

--- src/main/java/com/willyes/clemenintegra/inventario/dto/LotePendienteUbicarProjection.java
+++ src/main/java/com/willyes/clemenintegra/inventario/dto/LotePendienteUbicarProjection.java
@@ -1,20 +1,32 @@
 package com.willyes.clemenintegra.inventario.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public interface LotePendienteUbicarProjection {
     Long getLoteId();
 
     String getCodigoLote();
 
-    Integer getProductoId();
+    Long getProductoId();
 
     String getNombreProducto();
 
     String getTipoProducto();
 
-    String getNombreAlmacen();
-
     BigDecimal getStockDisponible();
-}
 
+    LocalDateTime getFechaVencimiento();
+
+    String getEstado();
+
+    Long getAlmacenIdActual();
+
+    String getNombreAlmacenActual();
+
+    Long getAlmacenDestinoSugeridoId();
+
+    String getNombreAlmacenDestinoSugerido();
+
+    Boolean getRequiereUbicacionDestino();
+}
