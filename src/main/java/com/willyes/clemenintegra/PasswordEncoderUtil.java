9a953826fc59b73f9024c6351549a41b12087d0f
Yuseth Edwin Serrano Acevedo <edwseth.77@gmail.com>
2026-04-06 12:03:02 -0500

    Se actualiza la rama

--- src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
+++ src/main/java/com/willyes/clemenintegra/PasswordEncoderUtil.java
@@ -9,7 +9,7 @@ public class PasswordEncoderUtil {
 
     public static void main(String[] args) {
         // Cambia esta línea con la contraseña que quieres codificar
-        String rawPassword = "263419";
+        String rawPassword = "Capo2026$";
 
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         String encodedPassword = encoder.encode(rawPassword);
